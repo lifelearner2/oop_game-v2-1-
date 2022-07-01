1641f3bbe1806e04c3cabaf817f079a648bb4160
lifelearner2 <105907575+lifelearner2@users.noreply.github.com>
2022-07-01 01:27:45 -0700

    Update app.js
    added event listener

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@
 // console.log(`Phrase - phrase: ${phrase.phrase}`);
 
 //testing random phrases:
-const game = new Game();
+//const game = new Game();
 
 // game.phrases.forEach( (phrase, index) => {
 //     console.log(`Phrase ${index} - phrase: ${phrase.phrase}`);
@@ -20,9 +20,9 @@ const game = new Game();
 
 //testing the random selection of phrases from game.js
 //this may not show all five phrases to the console as it is showing phrases at random so you may see a phrase more than once.
-const logPhrase = (phrase) => {
-    console.log(`Phrase - phrase: `, phrase.phrase);
-};
+// const logPhrase = (phrase) => {
+//     console.log(`Phrase - phrase: `, phrase.phrase);
+//};
 // const game = new Game();
 
 // logPhrase(game.getRandomPhrase());
@@ -33,16 +33,25 @@ const logPhrase = (phrase) => {
 
 //Testing `addPhraseToDisplay` method
 //const game = new Game();
-game.getRandomPhrase().addPhraseToDisplay();  //returns an object that was created by calling Phrase class's constructor new Phrase
+//game.getRandomPhrase().addPhraseToDisplay();  //returns an object that was created by calling Phrase class's constructor new Phrase
  //or
 //const game = new Game();
- const randomPhrase = game.getRandomPhrase();
- const phrase = new Phrase(randomPhrase.phrase);
- phrase.addPhraseToDisplay();
+//  const randomPhrase = game.getRandomPhrase();
+//  const phrase = new Phrase(randomPhrase.phrase);
+//  phrase.addPhraseToDisplay();
 
  //Testing `start game()` method
  //const game = new Game();
-game.startGame();
-console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+//const game = new Game();
+//Step 8: declaring/defining var startButton with the id of btn_reset from the html
+const startButton = document.getElementById("btn_reset");
+//game.startGame();
+//console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+//created event listener for the start button. When it's clicked the game will start.
+startButton.addEventListener( "click", () => {
+    const game = new Game;
+    //if (e.target.tagName === "BUTTON") {
+        game.startGame(); 
+    });
 
-
+  
