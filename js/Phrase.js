c04bf8fabf057a24b77796484ab71dbfe6e9dc4c
lifelearner2 <105907575+lifelearner2@users.noreply.github.com>
2022-07-04 00:59:01 -0700

    Update Phrase.js
    updated comment

--- js/Phrase.js
+++ js/Phrase.js
@@ -54,7 +54,7 @@ class Phrase {
    * @param (string) letter - Letter to display
    */
   //showMatchedLetter method reveals the letter(s) on the board that matches the player's selection. not sure this is working.
-  //To reveal matching letter(s), select all of the letter DOM elements that have a CSS class name that matches the selected letter(li) and replace each selected hide CSS class with 'show'Class
+  //To reveal matching letter(s), select all of the letter DOM elements that have a CSS class name that matches the selected letter and replace each selected hide CSS class with 'show'Class
   showMatchedLetter(letter) {
     let revealLetter = document.getElementsByClassName(letter);
     for (let i = 0; i < revealLetter.length; i++) {
